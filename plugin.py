fcd284a622f193f71f2c23987643d1447a972167
janusz <janexpl@me.com>
2020-03-08 13:30:20 +0100

    Some fixes

--- plugin.py
+++ plugin.py
@@ -79,7 +79,7 @@ class BasePlugin:
                         novar = False
                         break
             if novar:
-                Domoticz.Debug("User Variable {} does not exist. Creation requested".format(varname), "Verbose")
+                Domoticz.Debug("User Variable {} does not exist. Creation requested".format(varname))
                 DomoticzAPI("type=command&param=saveuservariable&vname={}&vtype=2&vvalue={}".format(varname, str(
                     self.CWUPump)))
             else:
